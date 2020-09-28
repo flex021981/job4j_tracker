05d2c503ead82374fcbcb446b8f8390cf64e247b
Aleksey Zakharov <flex021981@gmail.com>
2020-09-28 13:55:08 +0300

    1.1. Создание объекта [#369368]

---
+++ src/main/java/ru/job4j/oop/Dog.java
@@ -0,0 +1,9 @@
+package ru.job4j.oop;
+
+public class Dog {
+    public static void main(String[] args) {
+        Dog barbos = new Dog();
+        Dog bobik = new Dog();
+        Dog sharik = new Dog();
+    }
+}
