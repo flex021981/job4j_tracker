070ad8f6383935c2e41eee320fc3e97857989734
Aleksey Zakharov <flex021981@gmail.com>
2020-09-28 15:33:47 +0300

    1.7. Состояние объекта [#369374]

---
+++ src/main/java/ru/job4j/oop/Battery.java
@@ -0,0 +1,30 @@
+package ru.job4j.oop;
+
+public class Battery {
+    private int load;
+
+    public Battery(int load) {
+        this.load = load;
+    }
+
+    public int getLoad() {
+        return load;
+    }
+
+    public void setLoad(int load) {
+        this.load = load;
+    }
+
+    public void exchange(Battery another) {
+        load = load - 10;
+        another.setLoad(another.getLoad() + 10);
+    }
+
+    public static void main(String[] args) {
+        Battery batOne = new Battery(30);
+        Battery batTwo = new Battery(10);
+        batOne.exchange(batTwo);
+        System.out.println(batOne.getLoad());
+        System.out.println(batTwo.getLoad());
+    }
+}
