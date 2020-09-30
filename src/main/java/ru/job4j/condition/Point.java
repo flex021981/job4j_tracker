fd131e549d499355f9dd0fe29d9e34fa398a508a
Aleksey Zakharov <flex021981@gmail.com>
2020-09-30 14:09:25 +0300

    4. Расстояние между точками в трехмерном пространстве. [#369366]

---
+++ src/main/java/ru/job4j/condition/Point.java
@@ -0,0 +1,49 @@
+package ru.job4j.condition;
+
+import static java.lang.Math.sqrt;
+import static java.lang.Math.pow;
+
+public class Point {
+
+    private int x;
+    private int y;
+    private int z;
+
+    /**
+     * Конструтор, который принимает начальное состояние объекта "точка" 2D
+     *
+     * @param first  координата x
+     * @param second координата y
+     */
+    public Point(int first, int second) {
+        this.x = first;
+        this.y = second;
+    }
+
+    /**
+     * Конструтор, который принимает начальное состояние объекта "точка" 3D
+     *
+     * @param x координата x
+     * @param y координата y
+     * @param z координата z
+     */
+
+    public Point(int x, int y, int z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    public double distance(Point that) {
+        return sqrt(pow(this.x - that.x, 2) + pow(this.y - that.y, 2));
+    }
+
+    public double distance3d(Point that) {
+        return sqrt(pow(this.x - that.x, 2) + pow(this.y - that.y, 2) + pow(this.z - that.z, 2));
+    }
+
+    public void info() {
+        System.out.println(String.format("Point[%s, %s]", this.x, this.y));
+    }
+}
+
---
+++ src/test/java/ru/job4j/condition/PointTest.java
@@ -0,0 +1,54 @@
+package ru.job4j.condition;
+
+import junit.framework.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PointTest {
+    @Test
+    public void when00to20then2() {
+        Point pointOne = new Point(0, 0);
+        Point pointTwo = new Point(2, 0);
+        int expected = 2;
+        double out = pointOne.distance(pointTwo);
+        Assert.assertEquals(expected, out, 0.01);
+    }
+
+    @Test
+    public void when00to02then2() {
+        Point pointOne = new Point(0, 0);
+        Point pointTwo = new Point(0, 2);
+        Assert.assertEquals(2, pointOne.distance(pointTwo), 0.01);
+    }
+
+    @Test
+    public void when02to00then2() {
+        Point pointOne = new Point(0, 2);
+        Point pointTwo = new Point(0, 0);
+        Assert.assertEquals(2, pointOne.distance(pointTwo), 0.01);
+    }
+
+    @Test
+    public void when20to00then2() {
+        Point pointOne = new Point(2, 0);
+        Point pointTwo = new Point(0, 0);
+        Assert.assertEquals(2, pointOne.distance(pointTwo), 0.01);
+    }
+
+    @Test
+    public void d3when002to000then2() {
+        Point pointOne = new Point(0, 0, 2);
+        Point pointTwo = new Point(0, 0, 0);
+        Assert.assertEquals(2, pointOne.distance3d(pointTwo), 0.01);
+    }
+
+    @Test
+    public void d3when000to002then2() {
+        Point pointOne = new Point(0, 0, 0);
+        Point pointTwo = new Point(0, 0, 2);
+        Assert.assertEquals(2, pointOne.distance3d(pointTwo), 0.01);
+    }
+
+
+}
