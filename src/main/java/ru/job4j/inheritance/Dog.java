a11983c9225f4aac7914e7ee056f0ffb9afe12af
Aleksey Zakharov <flex021981@gmail.com>
2020-09-30 15:26:30 +0300

    1. Что такое наследование. [#369386]

---
+++ src/main/java/ru/job4j/inheritance/Dog.java
@@ -0,0 +1,7 @@
+package ru.job4j.inheritance;
+
+public class Dog {
+    public void single(){
+        System.out.println("ГАВ-ГАВ");
+    }
+}
---
+++ src/main/java/ru/job4j/inheritance/ToyDog.java
@@ -0,0 +1,9 @@
+package ru.job4j.inheritance;
+
+public class ToyDog extends Dog {
+
+    public static void main(String[] args) {
+        ToyDog bobik = new ToyDog();
+        bobik.single();
+    }
+}
