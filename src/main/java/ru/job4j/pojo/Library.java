72448050c11dce60ec5a713b94788cca043c2f03
Aleksey Zakharov <flex021981@gmail.com>
2020-10-01 14:31:18 +0300

    3. Массивы и модели. [#369390]

---
+++ src/main/java/ru/job4j/pojo/Library.java
@@ -0,0 +1,38 @@
+package ru.job4j.pojo;
+
+public class Library {
+
+    public static void main(String[] args) {
+        Book bookOne = new Book("Clean code", 256);
+        Book bookTwo = new Book("Alphabet", 26);
+        Book bookThree = new Book("Алфавит", 33);
+        Book bookFour = new Book("Learn Java", 1024);
+        Book bookTemp = new Book();
+
+        Book[] arrBooks = new Book[4];
+        arrBooks[0] = bookOne;
+        arrBooks[1] = bookTwo;
+        arrBooks[2] = bookThree;
+        arrBooks[3] = bookFour;
+
+        for (int i = 0; i < arrBooks.length; i++) {
+            System.out.println(arrBooks[i].toString() + " Индекс - " + i);
+        }
+
+        System.out.println();
+        bookTemp = arrBooks[0];
+        arrBooks[0] = arrBooks[3];
+        arrBooks[3] = bookTemp;
+        for (Book book : arrBooks) {
+            System.out.println(book.toString());
+        }
+
+        System.out.println();
+        for (Book book : arrBooks) {
+            if (book.getName() == "Clean code") {
+                System.out.println(book.toString());
+            }
+        }
+
+    }
+}
