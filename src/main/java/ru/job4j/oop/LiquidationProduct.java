e424f6e57f5e4bb5a085ec6684f475083fc0fcef
Aleksey Zakharov <flex021981@gmail.com>
2020-10-01 12:04:00 +0300

    6. Почему наследование - это плохо [#369384]

---
+++ src/main/java/ru/job4j/oop/LiquidationProduct.java
@@ -0,0 +1,21 @@
+package ru.job4j.oop;
+
+public final class LiquidationProduct {
+    private Product product;
+
+    public LiquidationProduct(String name, int price) {
+        product = new Product(name, price);
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public int discount() {
+        return product.discount();
+    }
+
+    public String label() {
+        return product.label();
+    }
+}
