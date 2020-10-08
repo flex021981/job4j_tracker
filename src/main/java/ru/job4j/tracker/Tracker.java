ff3e32903339e82135bc4a86095bd706cad2d2fd
Алексей Захаров <flex021981@gmail.com>
2020-10-08 19:04:28 +0300

    7. Метод удаления заявки Tracker.delete [#369394]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -59,11 +59,21 @@ public class Tracker {
 
     public boolean replace(int id, Item item) {
         int index = indexOf(id);
-        if (index == -1) {
-            return false;
-        } else {
-            items[index].setName(item.getName());
+        if (index != -1) {
+            item.setId(items[index].getId());
+            items[index] = item;
             return true;
+        } else {
+            return false;
         }
+    }
+
+    public boolean delete(int id) {
+        int index = indexOf(id);
+        items[index] = null;
+        System.arraycopy(items, index + 1, items, index, items.length - index - 1);
+        items[size - 1] = null;
+        size--;
+        return true;
     }
 }
