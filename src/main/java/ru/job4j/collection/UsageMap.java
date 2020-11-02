30d276b0bfe90d18820af30c7390ba8fe4aaa1aa
Алексей Захаров <flex021981@gmail.com>
2020-11-02 11:35:10 +0300

    0. Map, HashMap [#369304]

---
+++ src/main/java/ru/job4j/collection/UsageMap.java
@@ -0,0 +1,30 @@
+package ru.job4j.collection;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class UsageMap {
+    public static void main(String[] args) {
+        HashMap<String, String> map = new HashMap<>();
+        map.put("parsentev@yandex.ru", "Petr Arsentev");
+
+        for (String key : map.keySet()) {
+            String value = map.get(key);
+            System.out.println(key + " = " + value);
+        }
+
+        map.put("parsentev@yandex.ru", "Petr Arsentev Sergeevich");
+        for (Map.Entry<String, String> entry : map.entrySet()) {
+            String key = entry.getKey();
+            String value = entry.getValue();
+            System.out.println(key + " = " + value);
+        }
+
+        map.remove("parsentev@yandex.ru");
+        map.put("parsentev1@yandex.ru", "Petr Arsentev");
+        for (String key : map.keySet()) {
+            String value = map.get(key);
+            System.out.println(key + " = " + value);
+        }
+    }
+}
