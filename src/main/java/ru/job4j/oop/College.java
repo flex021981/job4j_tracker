bf38b1ffac0fbabd3830693605bd6510e93a7c59
Алексей Захаров <flex021981@gmail.com>
2020-11-03 15:17:49 +0300

    0. Checkstyle с Maven [#369282]

--- src/main/java/ru/job4j/oop/College.java
+++ src/main/java/ru/job4j/oop/College.java
@@ -4,15 +4,20 @@ public class College {
     public static void main(String[] args) {
         Freshman freshman = new Freshman();
 
-        Student student = freshman; // Восходящее перебразование  Freshman к Student
+        // Восходящее перебразование  Freshman к Student
+        Student student = freshman;
 
-        Object objFreshman = new Freshman(); // Восходящее перебразование Freshman к Object
-        Object objStudent = new Student(); // Восходящее перебразование Student к Object
+        // Восходящее перебразование Freshman к Object
+        Object objFreshman = new Freshman();
+        // Восходящее перебразование Student к Object
+        Object objStudent = new Student();
 
-        Student studentFromObj = (Student) objStudent; //Нисходящее переобразование Object к Student
-        Freshman freshmanFromObj = (Freshman) objFreshman; //Нисходящее переобразование Object к Freshman
+        //Нисходящее переобразование Object к Student
+        Student studentFromObj = (Student) objStudent;
+        //Нисходящее переобразование Object к Freshman
+        Freshman freshmanFromObj = (Freshman) objFreshman;
 
-        Freshman freshmanFromStudent = (Freshman) student; //Нисходящее переобразование Student к Freshman
+        //Нисходящее переобразование Student к Freshman
+        Freshman freshmanFromStudent = (Freshman) student;
     }
-
 }
