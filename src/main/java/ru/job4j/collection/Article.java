f58a7b345e37d0a74cad2057ac819b4fd50180d6
Алексей Захаров <flex021981@gmail.com>
2020-11-11 10:15:38 +0300

    1. Генератор текста - Упражнения

---
+++ src/main/java/ru/job4j/collection/Article.java
@@ -0,0 +1,13 @@
+package ru.job4j.collection;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Article {
+    public static boolean generateBy(String origin, String line) {
+        origin = origin.replaceAll("\\p{Punct}", "");
+        List<String> originList = Arrays.asList(origin.toLowerCase().split(" "));
+        List<String> lineList = Arrays.asList(line.toLowerCase().split(" "));
+        return originList.containsAll(lineList);
+    }
+}
