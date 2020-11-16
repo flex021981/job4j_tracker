0ba927db89ca3aa651f6e099dbde2142ee5f2f7b
Алексей Захаров <flex021981@gmail.com>
2020-11-16 10:45:22 +0300

    fix code style

--- src/main/java/ru/job4j/lambda/Reduce.java
+++ src/main/java/ru/job4j/lambda/Reduce.java
@@ -5,7 +5,8 @@ import java.util.function.BinaryOperator;
 import java.util.function.Supplier;
 
 public class Reduce {
-    private static int loop(int to, BiFunction<Integer, Integer, Integer> func, Supplier<Integer> initValue) {
+    private static int loop(int to, BiFunction<Integer, Integer, Integer> func,
+                            Supplier<Integer> initValue) {
         int rsl = initValue.get();
         for (int index = 1; index <= to; index++) {
             rsl = func.apply(rsl, index);
